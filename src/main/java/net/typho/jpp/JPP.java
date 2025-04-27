27c4abd690c471d2c8772fbdc090df0036f909a5
evan <thetyphothanian@gmail.com>
2025-04-27 10:14:27 -0600

    trying to get ELF hello world working but it's not

--- src/main/java/net/typho/jpp/JPP.java
+++ src/main/java/net/typho/jpp/JPP.java
@@ -5,7 +5,7 @@ import net.typho.jpp.lexical.StringLexer;
 import net.typho.jpp.parsing.DefaultParser;
 import net.typho.jpp.parsing.Parser;
 
-import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -39,7 +39,15 @@ public class JPP {
     }
 
     public static void main(String[] args) throws IOException {
-        parse(Path.of("src", "main", "java", "net", "typho", "jpp"));
-        parse(new File("D:\\_code").toPath());
+        //parse(Path.of("src", "main", "java", "net", "typho", "jpp"));
+        //parse(new File("D:\\_code").toPath());
+
+        try (FileOutputStream out = new FileOutputStream("header.bin")) {
+            String header = "7F454C4602010100000000000000000002003E00010000008000400000000000400000000000000000000000000000000000000000000000400038000100400000000000000100000005000000800000000000000080004000000000008000400000000000380000000000000038000000000000000000000010000000000000";
+
+            for (int i = 0; i < header.length(); i += 2) {
+                out.write(Byte.parseByte(String.valueOf(header.charAt(i)) + header.charAt(i + 1), 16));
+            }
+        }
     }
 }
